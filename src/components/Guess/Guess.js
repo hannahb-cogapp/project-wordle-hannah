2542b4f46c1250d16a904469bf699b4d57e8a8de
Hannah Baker <hannahb@cogapp.com>
2023-10-25 10:12:33 +0100

    Initial solution to exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const letterStatus = value ? checkGuess(value, answer) : undefined;
+  
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span key={num} className={ letterStatus ? letterStatus[num].status + ' cell' : 'cell' }>
           {value ? value[num] : undefined}
         </span>
       ))}
